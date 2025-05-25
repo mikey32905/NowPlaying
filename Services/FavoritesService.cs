f32873ab0a07467f7e5ae72575637294f62354d9
Mike Williams <mikey32905@gmail.com>
2025-05-25 14:13:02 -0400

    added in the popular movies page and the favorites page. Storing favorites to localstorage.

---
+++ Services/FavoritesService.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.JSInterop;
+using NowPlaying.Models;
+using System.Text.Json;
+
+namespace NowPlaying.Services
+{
+    public class FavoritesService(IJSRuntime jSRuntime)
+    {
+        private readonly string _localStorageKey = "favorites";
+
+        /// <summary>
+        /// Returns a list of favorite movies from local storage.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Movie>> GetFavorites()
+        {
+            List<Movie> movies = [];
+
+            try
+            {
+                var json = await jSRuntime.InvokeAsync<string>("localStorage.getItem", _localStorageKey);
+                movies = JsonSerializer.Deserialize<List<Movie>>(json) ?? [];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error retrieving favorites: {ex.Message}");
+            }
+
+            return movies;
+        }
+
+        /// <summary>
+        /// Saves a list of movies to local storage.
+        /// </summary>
+        /// <param name="movies">a list of movies</param>
+        /// <returns></returns>
+        public async Task SaveFavorites(List<Movie> movies)
+        {
+            try
+            {
+                var json = JsonSerializer.Serialize(movies);
+                await jSRuntime.InvokeVoidAsync("localStorage.setItem", _localStorageKey, json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving favorites: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Adds a movie to the favorites list if it is not already present.
+        /// </summary>
+        /// <param name="movie">a movie item</param>
+        /// <returns></returns>
+        public async Task AddFavorite(Movie movie)
+        {
+            var currentMovies = await GetFavorites();
+
+            if (currentMovies.All(m => m.Id != movie.Id))
+            {
+                currentMovies.Add(movie);
+                await SaveFavorites(currentMovies);
+            }
+        }
+
+        /// <summary>
+        /// Removes a movie from the favorites list in storage.
+        /// </summary>
+        /// <param name="movie">a movie item</param>
+        /// <returns></returns>
+        public async Task RemoveFavorite(Movie movie)
+        {
+            var currentMovies = await GetFavorites();
+            currentMovies = currentMovies.Where(f => f.Id != movie.Id).ToList();
+
+            await SaveFavorites(currentMovies);
+        }
+
+        /// <summary>
+        /// checks to see if movie is in list of favorites
+        /// </summary>
+        /// <param name="id">Movie Id</param>
+        /// <returns></returns>
+        public async Task<bool> IsFavorite(int id)
+        {
+            var currentMovies = await GetFavorites();
+
+            bool IsFavorite = currentMovies.Any(f => f.Id == id);
+
+            return IsFavorite;
+        }
+
+    }
+}
---
+++ Services/TMDBService.cs
@@ -0,0 +1,76 @@
+﻿using NowPlaying.Models;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace NowPlaying.Services
+{
+    public class TMDBService
+    {
+        private readonly HttpClient _http;
+
+        private readonly JsonSerializerOptions _jsonOptions = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+        };
+
+        public TMDBService(HttpClient http, IConfiguration config)
+        {
+            _http = http;
+
+            string? tmdbKey = config["TmdbAccessKey"];
+
+            if (!string.IsNullOrWhiteSpace(tmdbKey))
+            {
+                _http.DefaultRequestHeaders.Authorization = new("Bearer", tmdbKey);
+            }
+        }
+
+        public async Task<MovieListResponse> GetNowPlayingMovies()
+        {
+            string url = "https://api.themoviedb.org/3/movie/now_playing?region=US&language=en-US";
+            string imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+
+            MovieListResponse response = await _http.GetFromJsonAsync<MovieListResponse>(url, _jsonOptions)
+                ?? throw new HttpIOException(HttpRequestError.InvalidResponse, "Now Showing Movies could not be loaded.");
+
+            foreach (var movie in response.Results)
+            {
+                if (string.IsNullOrEmpty(movie.PosterPath))
+                {
+                    movie.PosterPath = "/img/mw1920_poster.png";
+                }
+                else
+                {
+                    movie.PosterPath = $"{imageBaseUrl}{movie.PosterPath}";
+                }
+            }
+
+            return response;
+        }
+
+        public async Task<MovieListResponse> GetPopularMovies()
+        {
+            string url = "https://api.themoviedb.org/3/movie/popular?region=US&language=en-US";
+            string imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+
+            MovieListResponse response = await _http.GetFromJsonAsync<MovieListResponse>(url, _jsonOptions)
+            ?? throw new HttpIOException(HttpRequestError.InvalidResponse, "Popular Movies could not be loaded.");
+
+            foreach (var movie in response.Results)
+            {
+                if (string.IsNullOrEmpty(movie.PosterPath))
+                {
+                    movie.PosterPath = "/img/mw1920_poster.png";
+                }
+                else
+                {
+                    movie.PosterPath = $"{imageBaseUrl}{movie.PosterPath}";
+                }
+            }
+
+            return response;
+
+        }
+
+    }
+}
